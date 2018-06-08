6d1de82c4060afa56dc734e2176dbc0bec8fc231
ZCOLT <colton.zac@gmail.com>
2018-06-07 22:02:27 -0600

    added moment.js

--- assets/main.js
+++ assets/main.js
@@ -8,9 +8,10 @@ var config = {
     storageBucket: "",
     messagingSenderId: "887123173851"
   };
+
   firebase.initializeApp(config);
 
-  var database = firbase.database();
+  var database = firebase.database();
   
   $('#add-train').on("click", function (event){
       event.preventDefault();
@@ -41,3 +42,37 @@ var config = {
         $("#time-input").val("");
         $("#frequency-input").val("");
   })
+
+  database.ref().on("child_added", function (snapshot) {
+    var destination = snapshot.val().destination;
+   
+    var name = snapshot.val().name;
+    console.log(destination, name);
+    // Assumptions
+    var tFrequency = snapshot.val().frequency;
+    // Time is 3:30 AM
+    var firstTime = snapshot.val().firstTime;;
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    var arivalTime = moment(nextTrain).format("hh:mm")
+
+      // Handle the errors
+  }, function (errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+  });
